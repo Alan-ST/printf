32e1b13db5ff19cda4bc08a5e93e39c74b8024e8
Alan-ST <alan.gon138383@gmail.com>
2021-07-26 18:13:47 -0500

    printf

--- _print.c
+++ _print.c
@@ -9,13 +9,10 @@ int _print(const char *format, check_t functions[], va_list args)
 {
 	int i, j, val, prnt = 0;
 
-	/*checks the format string*/
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		/*checks the format specifier*/
 		if (format[i] == '%')
 		{
-			/*search up the right function*/
 			for (j = 0; functions[j].character != NULL; j++)
 			{
 				if (format[i + 1] == functions[j].character[0])
@@ -29,7 +26,10 @@ int _print(const char *format, check_t functions[], va_list args)
 			}
 			if (functions[j].character == NULL && format[i + 1] != ' ')
 			{
-				if (format)
+				if (format[i + 1] != '\0')
+				{
+
+				}
 			}
 		}
 		else
