cdf2b6e40351163907d9b76d28db16bd017576d2
Alan-ST <alan.gon138383@gmail.com>
2021-07-26 17:53:33 -0500

    printf

--- _print.c
+++ _print.c
@@ -7,4 +7,20 @@
  */
 int _print(const char *format, check_t functions[], va_list args)
 {
+	int i, j, val, prnt = 0;
+
+	/*checks the format string*/
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		/*checks the format specifier*/
+		if (format[i] == '%')
+		{
+			/*search up the right function*/
+			for (j = 0; functions[j].character != NULL; j++)
+			{
+				if (format[i + 1] == functions[j].character[0])
+			}
+		}
+		else
+	}
 }
