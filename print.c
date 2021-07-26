92cbb879b1f6e15bf929bef82fe02cfc3aef72d9
Alan-ST <alan.gon138383@gmail.com>
2021-07-26 18:33:21 -0500

    printf

--- print.c
+++ print.c
@@ -7,41 +7,19 @@
  */
 int _print(const char *format, check_t functions[], va_list args)
 {
-	int i, j, val, prnt = 0;
+	int i = 0, j = 0, val = 0;
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (format != NULL)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; functions[j].character != NULL; j++)
-			{
-				if (format[i + 1] == functions[j].character[0])
-				{
-					val = functions[j].f(args);
-					if (val == -1)
-						return (-1);
-					prnt += val;
-					break;
-				}
-			}
-			if (functions[j].character == NULL && format[i + 1] != ' ')
-			{
-				if (format[i + 1] != '\0')
-				{
-					putachar(format[i]);
-					putachar(format[i + 1]);
-					prnt += 2;
-				}
-				else
-					return (-1);
-			}
-			i += 1;
-		}
-		else
-		{
-			putachar(format[i]);
-			prnt++;
+			if (format[i + 1] == '\0')
+				return (-1);
+			if (format[i + 1] == ' ')
+				i++;
+
+			while (j < 4)
+			{}
 		}
 	}
-	return (prnt);
 }
