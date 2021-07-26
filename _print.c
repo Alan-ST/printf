d5da25d70476565e4f2a18fbdc4eaeead025a542
Alan-ST <alan.gon138383@gmail.com>
2021-07-26 17:32:12 -0500

    printf

---
+++ _print.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _print - prints the format
+ * @format: arguments
+ * @functions: function's list
+ * @args: argurment's list
+ */
+int _print(const char *format, check_t functions[], va_list args)
+{
+}
