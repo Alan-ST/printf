4f2c38f901c2d7054882fd019dfe097001400b6b
Alan-ST <alan.gon138383@gmail.com>
2021-07-28 14:38:30 -0500

    printf

--- print.c
+++ print.c
@@ -10,22 +10,17 @@ int print(const char *format, check_t functions[], va_list args)
 {
 	int i = 0, j = 0, val = 0;
 
-	/*Checks the format string meanwhile isnt NULL*/
 	while (format != NULL)
 	{
-		/*Check type variable/specifier*/
 		if (format[i] == '%')
 		{
-			/*Check if the next index isnt NULL*/
 			if (format[i + 1] == '\0')
 				return (-1);
 			if (format[i + 1] == ' ')
 				i++;
 
-			/*Search up the right function*/
 			while (j < 4)
 			{
-				/*Successful function*/
 				if (format[i + 1] == functions[j].character[0])
 				{
 					val += functions[j].f(args);
